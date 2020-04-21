a763f0f6180d979826ec00b6fc9b36920fa52020
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2020-04-21 15:49:02 +0200

    Add logic to misc steps handle of error message

--- test/dk/dtu/SoftEngExamProjectG18/tests/MiscSteps.java
+++ test/dk/dtu/SoftEngExamProjectG18/tests/MiscSteps.java
@@ -1,12 +1,16 @@
 package dk.dtu.SoftEngExamProjectG18.tests;
 
+import dk.dtu.SoftEngExamProjectG18.tests.Util.TestHolder;
 import io.cucumber.java.en.Then;
+
+import static org.junit.Assert.assertTrue;
 
 public class MiscSteps {
 
     @Then("the error message {string} is given")
-    public void theErrorMessageIsGiven(String arg0) {
-
+    public void theErrorMessageIsGiven(String message) {
+        TestHolder testHolder = TestHolder.getInstance();
+        assertTrue(testHolder.response.exceptionMessageIs(message));
     }
 
 
