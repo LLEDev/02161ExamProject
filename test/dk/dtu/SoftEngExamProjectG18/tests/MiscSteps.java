946d361abe73375dc7594512bcfa317046b49786
Lasse Lund-Egmose <lle@lledev.com>
2020-04-21 17:10:34 +0200

    Reset CompanyDB before each scenario

--- test/dk/dtu/SoftEngExamProjectG18/tests/MiscSteps.java
+++ test/dk/dtu/SoftEngExamProjectG18/tests/MiscSteps.java
@@ -1,11 +1,18 @@
 package dk.dtu.SoftEngExamProjectG18.tests;
 
+import dk.dtu.SoftEngExamProjectG18.DB.CompanyDB;
 import dk.dtu.SoftEngExamProjectG18.tests.Util.TestHolder;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 
 import static org.junit.Assert.assertTrue;
 
 public class MiscSteps {
+
+    @Before // Reset CompanyDB before each scenario
+    public void beforeScenario(){
+        CompanyDB.initNewInstance();
+    }
 
     @Then("the error message {string} is given")
     public void theErrorMessageIsGiven(String message) {
